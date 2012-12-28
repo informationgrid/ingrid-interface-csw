bd447d1bf018e5358d34051c2d2ad535cff1d225
jm <jm@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2012-12-28 12:22:32 +0000

    - add specific test for https://dev2.wemove.com/jira/browse/INGRID-2194

--- src/test/java/de/ingrid/interfaces/csw/mapping/MappingTest.java
+++ src/test/java/de/ingrid/interfaces/csw/mapping/MappingTest.java
@@ -8,13 +8,16 @@ import java.util.Scanner;
 
 import junit.framework.TestCase;
 
+import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 
 import de.ingrid.interfaces.csw.domain.constants.ElementSetName;
 import de.ingrid.interfaces.csw.mapping.impl.XsltMapper;
+import de.ingrid.interfaces.csw.tools.StringUtils;
 import de.ingrid.utils.dsc.Record;
 import de.ingrid.utils.idf.IdfTool;
 import de.ingrid.utils.xml.IDFNamespaceContext;
+import de.ingrid.utils.xml.XMLUtils;
 import de.ingrid.utils.xpath.XPathUtils;
 
 /**
@@ -56,4 +59,34 @@ public class MappingTest extends TestCase {
 		XPathUtils xpath = new XPathUtils(new IDFNamespaceContext());
 		assertEquals("05F9A598-D866-11D2-AB09-00E0292DC06B", xpath.getString(result, "/gmd:MD_Metadata/gmd:fileIdentifier/gco:CharacterString"));
 	}
+	
+	   public void test_ISSUE_INGRID_2194() throws Exception {
+	        XsltMapper mapper = new XsltMapper();
+
+	        String idfContent = new Scanner(new File("src/test/resources/81A36D07-BD83-495A-8A94-30416165C86D.xml")).useDelimiter("\\A").next();
+	        Record record = IdfTool.createIdfRecord(idfContent, false);
+	        Node result = mapper.map(record, ElementSetName.FULL);
+
+	        XPathUtils xpath = new XPathUtils(new IDFNamespaceContext());
+	        
+	        
+	        /*
+	         * <gmd:CI_OnlineResource>
+<gmd:linkage>
+<gmd:URL>http://www.sachsen-anhalt.de/index.php?id=36252&lt;/gmd:URL>
+</gmd:linkage>
+<gmd:name>
+ <gco:CharacterString>Datenangebot</gco:CharacterString>
+</gmd:name>
+<gmd:function>
+<gmd:CI_OnLineFunctionCode codeList="http://www.tc211.org/ISO19139/resources/codeList.xml#CI_OnLineFunctionCode" codeListValue="Basic Data"/>
+</gmd:function>Basisdaten 
+	         */
+	        
+	        String xml = XMLUtils.toString((Document)result);
+	        System.out.println(xml);
+	        
+	        assertFalse(xpath.getString(result, "//gmd:CI_OnlineResource[gmd:function/gmd:CI_OnLineFunctionCode/@codeListValue='Basic Data']").contains("Basisdaten"));
+	    }
+
 }
