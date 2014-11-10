41aa6c4833214d8e2c0913e478bed5d106e6a911
aw <aw@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2014-11-10 16:26:06 +0000

    - added license header to all source files (REDMINE-524)

--- src/main/resources/idf_to_lucene.js
+++ src/main/resources/idf_to_lucene.js
@@ -1,3 +1,25 @@
+/*
+ * **************************************************-
+ * ingrid-interface-csw
+ * ==================================================
+ * Copyright (C) 2014 wemove digital solutions GmbH
+ * ==================================================
+ * Licensed under the EUPL, Version 1.1 or – as soon they will be
+ * approved by the European Commission - subsequent versions of the
+ * EUPL (the "Licence");
+ * 
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at:
+ * 
+ * http://ec.europa.eu/idabc/eupl5
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the Licence is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and
+ * limitations under the Licence.
+ * **************************************************#
+ */
 /**
  * CSW 2.0.2 AP ISO 1.0 Record (full) to Lucene Document mapping according to mapping IGC 1.0.3
  * Copyright (c) 2008 wemove digital solutions. All rights reserved.
