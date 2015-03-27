c02814ba9ba6b368ef03ffe90edda11867e03422
Toan Tran <khanh.toan.tran@wemove.com>
2015-03-27 19:12:47 +0100

    Check JAVA version to add vm option "-XX:+UseStringDeduplication" and set newRatio to 1.

--- src/main/release/scripts/start.sh
+++ src/main/release/scripts/start.sh
@@ -189,9 +189,26 @@ startIplug()
 
   # run it
   export CLASSPATH="$CLASSPATH"
-  INGRID_OPTS="$INGRID_OPTS -Dingrid_home=$INGRID_HOME -Dfile.encoding=UTF8 -XX:+UseG1GC -XX:+UseStringDeduplication -XX:NewRatio=3"
+  INGRID_OPTS="$INGRID_OPTS -Dingrid_home=$INGRID_HOME -Dfile.encoding=UTF8 -XX:+UseG1GC -XX:NewRatio=1"
   CLASS=de.ingrid.interfaces.csw.admin.JettyStarter
-	
+  
+  # check java version
+  JAVA_VERSION=`java -version 2>&1 |awk 'NR==1{ gsub(/"/,""); print $3 }'`
+  JAVA_VERSION_PART_0=`echo $JAVA_VERSION | awk '{split($0, array, "-")} END{print array[1]}'`
+  JAVA_VERSION_PART_1=`echo $JAVA_VERSION_PART_0 | awk '{split($0, array, "_")} END{print array[1]}'`
+  JAVA_VERSION_PART_2=`echo $JAVA_VERSION_PART_0 | awk '{split($0, array, "_")} END{print array[2]}'`
+  if [[ "$JAVA_VERSION_PART_1" > "1.7.0" ]]; then
+	LENGTH="${#JAVA_VERSION_PART_2}"
+	if [[ "$LENGTH" < "2" ]]; then
+		JAVA_VERSION_PART_2="0"$JAVA_VERSION_PART_2
+	fi
+	if [[ "$JAVA_VERSION_PART_2" > "19" ]]; then
+		INGRID_OPTS="$INGRID_OPTS -XX:+UseStringDeduplication"
+	elif [[ "$JAVA_VERSION_PART_1" > "1.8.0" ]]; then
+		INGRID_OPTS="$INGRID_OPTS -XX:+UseStringDeduplication"
+	fi
+  fi
+
   exec nohup "$JAVA" $INGRID_OPTS $CLASS > console.log &
   
   echo "jetty ($INGRID_HOME) started."
