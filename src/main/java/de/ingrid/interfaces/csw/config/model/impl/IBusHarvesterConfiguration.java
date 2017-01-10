231dc891d76f2839a1e7908fb4312da7e7b873a7
Andre Wallat <andre.wallat@wemove.com>
2017-01-10 11:49:50 +0100

    - updated license header

--- src/main/java/de/ingrid/interfaces/csw/config/model/impl/IBusHarvesterConfiguration.java
+++ src/main/java/de/ingrid/interfaces/csw/config/model/impl/IBusHarvesterConfiguration.java
@@ -2,7 +2,7 @@
  * **************************************************-
  * ingrid-interface-csw
  * ==================================================
- * Copyright (C) 2014 - 2016 wemove digital solutions GmbH
+ * Copyright (C) 2014 - 2017 wemove digital solutions GmbH
  * ==================================================
  * Licensed under the EUPL, Version 1.1 or – as soon they will be
  * approved by the European Commission - subsequent versions of the
