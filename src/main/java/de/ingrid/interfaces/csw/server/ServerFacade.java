b49bdd306014187b8cf374591f9d04f516ef0052
jm <joachim@wemove.com>
2017-10-23 17:07:32 +0200

    - enable docker image build - improve logging

--- src/main/java/de/ingrid/interfaces/csw/server/ServerFacade.java
+++ src/main/java/de/ingrid/interfaces/csw/server/ServerFacade.java
@@ -207,12 +207,12 @@ public class ServerFacade {
 
         } catch (Exception e) {
             try {
-                log.debug(e.getMessage(), e);
+                log.warn(e.getMessage(), e);
                 if (encodingImpl != null) {
                     encodingImpl.reportError(e);
                 }
             } catch (IOException ioe) {
-                log.error("Unable to send error message to client: " + ioe.getMessage());
+                log.error("Unable to send error message to client.", ioe);
             }
         }
     }
