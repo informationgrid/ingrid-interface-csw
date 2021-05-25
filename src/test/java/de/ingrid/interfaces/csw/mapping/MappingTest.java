9a121538142a2af26b0a13cb0055d7e439f4ece6
Joachim Müller <joachim.mueller@wemove.com>
2021-05-25 20:49:42 +0200

    Map GML3.2 elements back to GML (#2505)
    ATTN: THE NAME OF THE XSLT FILE HAS CHANGED!
    
    This is a backport for the changes in 5.8.1 to the GML namespace. All occurrences
    of namespace "http://www.opengis.net/gml/3.2" are set to "http://www.opengis.net/gml".
    
    ATTN: Existing elements of namespace "http://www.opengis.net/gml" in the source are
    ignored!
    
    see also #1876
    
    (cherry picked from commit 86aafe972647fea8b50c52b14abcb4e51cfb8734)

--- src/test/java/de/ingrid/interfaces/csw/mapping/MappingTest.java
+++ src/test/java/de/ingrid/interfaces/csw/mapping/MappingTest.java
@@ -187,10 +187,68 @@ public class MappingTest extends TestCase {
 	}
 
 
+	public void testFullHMDKNewGmlNamespaceTicket2505() throws Exception {
+		XsltMapper mapper = new XsltMapper();
+
+		ApplicationProperties.setProperty(ConfigurationKeys.IDF_2_FULL_PROCESSING_XSLT, "idf_1_0_0_to_iso_metadata_hmdk_5.8.1_4.6.9.xsl");
+
+		String idfContent = new Scanner(new File("src/test/resources/idf_to_full_hmdk_5.2.1_new_gml_namespace.xml")).useDelimiter("\\A").next();
+		Record record = IdfTool.createIdfRecord(idfContent, false);
+		Node result = mapper.mapFull(record);
+
+		String xml = XMLUtils.toString((Document) result);
+		System.out.println("\ntestFullHMDKSpecialTicket1531\n");
+		System.out.println(xml);
+
+		XPathUtils xpath = new XPathUtils(new IDFWithXSINamespaceContext());
+		assertNotNull(xpath.getString(result, "/gmd:MD_Metadata/@xsi:schemaLocation"));
+		assertEquals("069E1872-5EF3-475E-B426-CD86F23CA61C",
+				xpath.getString(result, "/gmd:MD_Metadata/gmd:fileIdentifier/gco:CharacterString"));
+		assertEquals("http://standards.iso.org/ittf/PubliclyAvailableStandards/ISO_19139_Schemas/resources/codelist/gmxCodelists.xml#MD_ScopeCode",
+				xpath.getString(result, "/gmd:MD_Metadata/gmd:hierarchyLevel/gmd:MD_ScopeCode/@codeList"));
+		assertEquals("http://standards.iso.org/ittf/PubliclyAvailableStandards/ISO_19139_Schemas/resources/codelist/gmxCodelists.xml#CI_RoleCode",
+				xpath.getString(result, "//gmd:CI_RoleCode/@codeList"));
+		assertEquals("http://standards.iso.org/ittf/PubliclyAvailableStandards/ISO_19139_Schemas/resources/codelist/gmxCodelists.xml#CI_DateTypeCode",
+				xpath.getString(result, "//gmd:CI_DateTypeCode/@codeList"));
+		assertEquals("http://standards.iso.org/ittf/PubliclyAvailableStandards/ISO_19139_Schemas/resources/codelist/gmxCodelists.xml#MD_MaintenanceFrequencyCode",
+				xpath.getString(result, "//gmd:MD_MaintenanceFrequencyCode/@codeList"));
+		assertEquals("http://standards.iso.org/ittf/PubliclyAvailableStandards/ISO_19139_Schemas/resources/codelist/gmxCodelists.xml#MD_SpatialRepresentationTypeCode",
+				xpath.getString(result, "//gmd:MD_SpatialRepresentationTypeCode/@codeList"));
+		assertEquals("http://standards.iso.org/ittf/PubliclyAvailableStandards/ISO_19139_Schemas/resources/codelist/gmxCodelists.xml#MD_CharacterSetCode",
+				xpath.getString(result, "//gmd:MD_CharacterSetCode/@codeList"));
+
+		assertEquals("EPSG 25832: ETRS89 / UTM Zone 32N",
+				xpath.getString(result, "//gmd:referenceSystemInfo[1]/gmd:MD_ReferenceSystem/gmd:referenceSystemIdentifier/gmd:RS_Identifier/gmd:code/gco:CharacterString"));
+		assertEquals("EPSG 4258: ETRS89 / geographisch",
+				xpath.getString(result, "//gmd:referenceSystemInfo[2]/gmd:MD_ReferenceSystem/gmd:referenceSystemIdentifier/gmd:RS_Identifier/gmd:code/gco:CharacterString"));
+		assertEquals("Nutzungsbedingungen: Datenlizenz Deutschland – Namensnennung – Version 2.0",
+				xpath.getString(result, "//gmd:resourceConstraints/gmd:MD_LegalConstraints/gmd:useLimitation/gco:CharacterString"));
+		assertEquals("license",
+				xpath.getString(result, "//gmd:resourceConstraints/gmd:MD_LegalConstraints/gmd:useConstraints[1]/gmd:MD_RestrictionCode/@codeListValue"));
+		assertEquals("otherRestrictions",
+				xpath.getString(result, "//gmd:resourceConstraints/gmd:MD_LegalConstraints/gmd:useConstraints[2]/gmd:MD_RestrictionCode/@codeListValue"));
+		assertEquals("Nutzungsbedingungen: Datenlizenz Deutschland – Namensnennung – Version 2.0",
+				xpath.getString(result, "//gmd:resourceConstraints/gmd:MD_LegalConstraints/gmd:otherConstraints[1]/gco:CharacterString"));
+		assertEquals("Quellenvermerk: Freie und Hansestadt Hamburg, Landesbetrieb Geoinformation und Vermessung",
+				xpath.getString(result, "//gmd:resourceConstraints/gmd:MD_LegalConstraints/gmd:otherConstraints[2]/gco:CharacterString"));
+		assertEquals("{\"id\":\"dl-by-de/2.0\",\"name\":\"Datenlizenz Deutschland – Namensnennung – Version 2.0\",\"url\":\"https://www.govdata.de/dl-de/by-2-0\",\"quelle\":\"Freie und Hansestadt Hamburg, Landesbetrieb Geoinformation und Vermessung\"}",
+				xpath.getString(result, "//gmd:resourceConstraints/gmd:MD_LegalConstraints/gmd:otherConstraints[3]/gco:CharacterString"));
+		assertEquals("Es gelten keine Zugriffsbeschränkungen",
+				xpath.getString(result, "//gmd:resourceConstraints[gmd:MD_LegalConstraints/gmd:accessConstraints/gmd:MD_RestrictionCode/@codeListValue='otherRestrictions'][1]/gmd:MD_LegalConstraints/gmd:otherConstraints/gco:CharacterString"));
+		assertEquals("Hamburgisches Geodateninfrastrukturgesetz (HmbGDIG)",
+				xpath.getString(result, "//gmd:resourceConstraints[gmd:MD_LegalConstraints/gmd:accessConstraints/gmd:MD_RestrictionCode/@codeListValue='otherRestrictions'][2]/gmd:MD_LegalConstraints/gmd:otherConstraints/gco:CharacterString"));
+		assertEquals("INSPIRE Richtlinie",
+				xpath.getString(result, "//gmd:resourceConstraints[gmd:MD_LegalConstraints/gmd:accessConstraints/gmd:MD_RestrictionCode/@codeListValue='otherRestrictions'][3]/gmd:MD_LegalConstraints/gmd:otherConstraints/gco:CharacterString"));
+		assertEquals("completeness omission", xpath.getString(result, "//gml:quantityType"));
+
+	}
+
 	public class IDFWithXSINamespaceContext extends IDFNamespaceContext {
 		public String getNamespaceURI(String prefix) {
 			if (prefix.equals("xsi")) {
 				return "http://www.w3.org/2001/XMLSchema-instance";
+			} else if (prefix.equals("gml")) {
+					return "http://www.opengis.net/gml";
 			} else {
 				return super.getNamespaceURI(prefix);
 			}
