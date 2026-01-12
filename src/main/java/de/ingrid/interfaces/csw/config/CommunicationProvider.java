84cfc02b21f355d6ea790915d62d40f19b7f7733
André Wallat <andre.wallat@wemove.com>
2026-01-12 11:01:38 +0100

    Update license header

--- src/main/java/de/ingrid/interfaces/csw/config/CommunicationProvider.java
+++ src/main/java/de/ingrid/interfaces/csw/config/CommunicationProvider.java
@@ -2,7 +2,7 @@
  * **************************************************-
  * ingrid-interface-csw
  * ==================================================
- * Copyright (C) 2014 - 2025 wemove digital solutions GmbH
+ * Copyright (C) 2014 - 2026 wemove digital solutions GmbH
  * ==================================================
  * Licensed under the EUPL, Version 1.1 or – as soon they will be
  * approved by the European Commission - subsequent versions of the
